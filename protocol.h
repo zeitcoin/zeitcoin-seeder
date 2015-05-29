2854422a8d53d3acd7541b91d6380607b2e58b30
Rent_a_Ray <domy@synthetic-records.com>
2015-05-29 12:32:52 +0200

    Update protocol.h

--- protocol.h
+++ protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 22788 : 12788;
+    return testnet ? 22788 : 44845;
 }
 
 //
